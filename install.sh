0b995e29e40415a12f7eca0feee8ca5f7b492e31
Simple IP <kaidenjclaflin@gmail.com>
2025-08-20 21:53:59 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,9 +14,9 @@ cp -r AgarVM/root/config/* Save
 
 json_file="AgarVM/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=AgarVM -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=AgarVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped AgarVM
+    docker run -d --name=agarvm -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=agarvm -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped agarvm
 else
-    docker run -d --name=AgarVM -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=AgarVM -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped AgarVM
+    docker run -d --name=agarvm -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=agarvm -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped agarvm
 fi
 clear
 echo "All finished! Check the port tab in your terminal you used to install this vm!"
