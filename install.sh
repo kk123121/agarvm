c82fa9e6ff4887170d9e36b6fee3d09b848032cc
Simple IP <kaidenjclaflin@gmail.com>
2025-08-20 22:01:25 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/kk123121/AgarVM
-cd AgarVM
+git clone https://github.com/kk123121/agarvm
+cd agarvm
 pip install textual
 sleep 2
 python3 installer.py
@@ -10,9 +10,9 @@ sudo apt update
 sudo apt install -y jq
 
 mkdir Save
-cp -r AgarVM/root/config/* Save
+cp -r agarvm/root/config/* Save
 
-json_file="AgarVM/options.json"
+json_file="agarvm/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
     docker run -d --name=agarvm -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=agarvm -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped agarvm
 else
